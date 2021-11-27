da1e4536c4937d3c195c967212b3065e505581ea
coconut-vanilla <80988520+coconut-vanilla@users.noreply.github.com>
2021-11-26 23:03:10 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+let strs = ["flower","flow","flight"];
+
+//var longestCommonPrefix = function (strs) {
+function Cad(strs) {
+
+        let prefix = '';
+
+        if (strs.length === 0) return prefix;
+        
+        for (let i = 0; i < strs[0].length; i++) {
+          const character = strs[0][i];
+          for (let j = 0; j < strs.length; j++) {
+            if (strs[j][i] !== character) return prefix;
+          }
+          prefix = prefix + character;
+        }
+        return prefix;  
+}
+
+cadena = Cad(strs);
+document.write(cadena);
+
+
+
