4d5d868eea499486d2ab4a2f42ac2b5b562379a1
Sam-dot-exe <78716644+Sam-dot-exe@users.noreply.github.com>
2021-12-05 16:50:24 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -50,7 +50,7 @@
       i++;
       if (i === longSt.length) {
         clearInterval(printStr)
-        document.getElementById("texto").style.color = 'green'
+        document.getElementById("texto").style.color = 'red'
       }
     }, 200);
   };
@@ -96,22 +96,7 @@
 
 }());
 
-//var longestCommonPrefix = function (strs) {
-function Cad() {
 
-  let prefix = '';
-
-  if (strs.length === 0) return prefix;
-
-  for (let i = 0; i < strs[0].length; i++) {
-    const character = strs[0][i];
-    for (let j = 0; j < strs.length; j++) {
-      if (strs[j][i] !== character) return prefix;
-    }
-    prefix = prefix + character;
-  }
-  return prefix;
-}
 
 
 
